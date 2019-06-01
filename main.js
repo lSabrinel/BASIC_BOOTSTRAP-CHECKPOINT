fe99d8d685664ec0214af6c6c042ebbb33fea1cc
Sabrine <49608453+lSabrinel@users.noreply.github.com>
2019-06-01 14:36:45 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+var e = document.getElementById('icon1');
+e.onmouseover = function() {
+  document.getElementById('text1').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('text1').style.display = 'none';
+}
+
+var e = document.getElementById('icon2');
+e.onmouseover = function() {
+  document.getElementById('text2').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('text2').style.display = 'none';
+}
+
+var e = document.getElementById('icon3');
+e.onmouseover = function() {
+  document.getElementById('text3').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('text3').style.display = 'none';
+}
+var e = document.getElementById('icon4');
+e.onmouseover = function() {
+  document.getElementById('text4').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('text4').style.display = 'none';
+}
+var e = document.getElementById('icon5');
+e.onmouseover = function() {
+  document.getElementById('text5').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('text5').style.display = 'none';
+}  
+
+var e = document.getElementById('icon6');
+e.onmouseover = function() {
+  document.getElementById('text6').style.display = 'block';
+}
+e.onmouseout = function() {
+  document.getElementById('text6').style.display = 'none';
+}
